91749721045d2679fdaf82486ca18152e38c1a4a
Christian Salemme <christiancarlossalemme@gmail.com>
2025-11-20 17:09:59 -0300

    Entrenamiento de carle

--- Jueguito/RacingGameCollision/MainJueguitoV2.py
+++ Jueguito/RacingGameCollision/MainJueguitoV2.py
@@ -24,7 +24,7 @@ GRASS = scale_image(pygame.image.load(SCRIPT_DIR / "imgs/grass.png"), 2.5)
 TRACK = scale_image(pygame.image.load(SCRIPT_DIR / "imgs/track2.png"), 0.9)
 
 TRACK_BORDER = scale_image(pygame.image.load(SCRIPT_DIR / "imgs/track-border2.png"), 0.9)
-TRACK_BORDER_MASK = pygame.mask.from_surface(TRACK_BORDER)
+TRACK_BORDER_MASK = pygame.mask.from_surface(TRACK_BORDER, threshold=220)
 
 FINISH = pygame.image.load(SCRIPT_DIR / "imgs/finish.png")
 FINISH_MASK = pygame.mask.from_surface(FINISH)
